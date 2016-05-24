67bee0ca73ca45701b36bfedd6f0b7ccb9bc251a
blead <blead010@gmail.com>
2016-05-25 01:00:33 +0700

    add criticalDamage, defense, finalDamage subcommands, optimize some calculations

--- plugins/dragonnest/dn-commands.js
+++ plugins/dragonnest/dn-commands.js
@@ -20,6 +20,10 @@ function dec(number,digits) {
   return Math.round(number*operand)/operand;
 }
 
+function calculatePercent(val,maxVal,cap) {
+  return Math.max(0,Math.min(val/maxVal,cap));
+}
+
 module.exports = {
   "help" : {
     desc : "Display this help message. If a subcommand is specified, give information about the subcommand.",
@@ -58,12 +62,12 @@ module.exports = {
 
       // crit: numerical value, percent: value between 0 and 1
       var crit,percent;
-      var level = (args.length==4) ? Number.parseInt(args[3]) : Constants.LEVEL;
+      var level = (args.length>=4) ? Number.parseInt(args[3]) : Constants.LEVEL;
       if(isNaN(level) || level < 1 || level > 100) {
         respond(true,"The specified level is invalid.");
         return;
       }
-      var messages = [];
+      var messages = [],critCap = Constants.CRITICAL_CAPS[level-1];
       messages.push("Level "+level+" critical");
 
       if(args[2].endsWith("%")) {
@@ -72,8 +76,8 @@ module.exports = {
           respond(true,"The specified value is invalid.");
           return;
         }
-        crit = Constants.CRITICAL_CAPS[level-1] * percent;
-        messages.push(": "+dec(crit)+" ("+dec(percent*100)+"%),");
+        crit = critCap * percent;
+        messages.push(": `"+dec(crit)+"` (`"+dec(percent*100)+"%`),");
 
       } else if(args[2] != "cap") {
         crit = parseSuffix(args[2]);
@@ -81,11 +85,146 @@ module.exports = {
           respond(true,"The specified value is invalid.");
           return;
         }
-        percent = Math.max(0,Math.min(crit/Constants.CRITICAL_CAPS[level-1],Constants.MAX_CRITICAL));
-        messages.push(": "+dec(crit)+" ("+dec(percent*100)+"%),");
+        percent = calculatePercent(crit,critCap,Constants.MAX_CRITICAL);
+        messages.push(": `"+dec(crit)+"` (`"+dec(percent*100)+"%`),");
       }
 
-      messages.push(" cap: "+dec(Constants.CRITICAL_CAPS[level-1]*Constants.MAX_CRITICAL)+" ("+(Constants.MAX_CRITICAL*100)+"%).");
+      messages.push(" cap: `"+dec(critCap*Constants.MAX_CRITICAL)+"` (`"+(Constants.MAX_CRITICAL*100)+"%`)");
+      respond(true,messages.join(""));
+    }
+  },
+  "criticalDamage" : {
+    desc : "Calculate in-game critical damage values.",
+    usage : "<value[%]/cap> [level]",
+    process : (args,respond) => {
+      if(args.length < 3) {
+        respond(true,"No value specified.");
+        return;
+      }
+
+      // critdmg: numerical value, percent: value between 0 and 1
+      var critdmg,percent;
+      var level = (args.length>=4) ? Number.parseInt(args[3]) : Constants.LEVEL;
+      if(isNaN(level) || level < 1 || level > 100) {
+        respond(true,"The specified level is invalid.");
+        return;
+      }
+      var messages = [],critdmgCap = Constants.CRITICAL_DAMAGE_CAPS[level-1];
+      messages.push("Level "+level+" critical damage");
+
+      if(args[2].endsWith("%")) {
+        percent = Number.parseFloat(args[2])/100 - 2;
+        if(percent < 0) {
+          respond(true,"The specified value is invalid.");
+          return;
+        }
+        critdmg = critdmgCap * percent;
+        percent += 2;
+        messages.push(": `"+dec(critdmg)+"` (`"+dec(percent*100)+"%`),");
+
+      } else if(args[2] != "cap") {
+        critdmg = parseSuffix(args[2]);
+        if(isNaN(critdmg)) {
+          respond(true,"The specified value is invalid.");
+          return;
+        }
+        percent = calculatePercent(critdmg,critdmgCap,Constants.MAX_CRITICAL_DAMAGE) + 2;
+        messages.push(": `"+dec(critdmg)+"` (`"+dec(percent*100)+"%`),");
+      }
+
+      messages.push(" cap: `"+dec(critdmgCap*Constants.MAX_CRITICAL_DAMAGE)+"` (`"+(Constants.MAX_CRITICAL_DAMAGE*100+200)+"%`)");
+      respond(true,messages.join(""));
+    }
+  },
+  "defense" : {
+    desc : "Calculate in-game defense values and effective HP for each damage type.",
+    usage : "HP P.Def M.Def [level]",
+    process : (args,respond) => {
+      if(args.length < 3) {
+        respond(true,"No value specified.");
+        return;
+      } else if(args.length < 5) {
+        respond(true,"Insufficient parameters. Use `"+args[0]+" help "+args[1]+"` for correct usage information.");
+        return;
+      }
+
+      var level = (args.length>=6) ? Number.parseInt(args[5]) : Constants.LEVEL;
+      if(isNaN(level) || level < 1 || level > 100) {
+        respond(true,"The specified level is invalid.");
+        return;
+      }
+      var defCap = Constants.DEFENSE_CAPS[level-1];
+
+      var rawHP = parseSuffix(args[2]), rawPDef = parseSuffix(args[3]), rawMDef = parseSuffix(args[4]);
+      if(isNaN(rawHP) || isNaN(rawPDef) || isNaN(rawMDef) || rawHP < 0 || rawPDef < 0 || rawMDef < 0) {
+        respond(true,"The specified values are invalid.");
+      }
+
+      var percentPDef = calculatePercent(rawPDef,defCap,Constants.MAX_DEFENSE);
+      var percentMDef = calculatePercent(rawMDef,defCap,Constants.MAX_DEFENSE);
+
+      var effectivePHP = rawHP/(1-percentPDef);
+      var effectiveMHP = rawHP/(1-percentMDef);
+      var messages = [];
+      messages.push("Level "+level+" defense:\n");
+      messages.push("defense cap: `"+dec(defCap*Constants.MAX_DEFENSE)+"` (`"+Constants.MAX_DEFENSE*100+"%`)\n");
+      messages.push("physical defense: `"+rawPDef+"` (`"+dec(percentPDef*100)+"%`)\n");
+      messages.push("magic defense: `"+rawMDef+"` (`"+dec(percentMDef*100)+"%`)\n");
+      messages.push("effective physical HP: `"+dec(effectivePHP)+"`\n");
+      messages.push("effective magical HP: `"+dec(effectiveMHP)+"`\n");
+      respond(true,messages.join(""));
+    }
+  },
+  "finalDamage" : {
+    desc : "Calculate in-game final damage values.",
+    usage : "<value[%]/cap> [level]",
+    process : (args,respond) => {
+      if(args.length < 3) {
+        respond(true,"No value specified.");
+        return;
+      }
+
+      // fd: numerical value, percent: value between 0 and 1
+      var fd,percent;
+      var level = (args.length>=4) ? Number.parseInt(args[3]) : Constants.LEVEL;
+      if(isNaN(level) || level < 1 || level > 100) {
+        respond(true,"The specified level is invalid.");
+        return;
+      }
+      var messages = [],fdCap = Constants.FINAL_DAMAGE_CAPS[level-1];
+      messages.push("Level "+level+" final damage");
+
+      if(args[2].endsWith("%")) {
+        percent = Number.parseFloat(args[2])/100;
+        if(percent < 0) {
+          respond(true,"The specified value is invalid.");
+          return;
+        }
+        if(percent < 0.146) {
+          fd = 2.857142857 * percent * fdCap;
+        } else {
+          fd = Math.pow(percent,1/2.2) * fdCap;
+        }
+        messages.push(": `"+dec(fd)+"` (`"+dec(percent*100)+"%`),");
+
+      } else if(args[2] != "cap") {
+        fd = parseSuffix(args[2]);
+        if(isNaN(fd)) {
+          respond(true,"The specified value is invalid.");
+          return;
+        }
+        var ratio = fd/fdCap;
+        if(ratio < 0.417) {
+          percent = (0.35*fd)/fdCap;
+        } else {
+          percent = Math.pow(ratio,2.2);
+        }
+
+        percent = calculatePercent(percent,1,Constants.MAX_FINAL_DAMAGE);
+        messages.push(": `"+dec(fd)+"` (`"+dec(percent*100)+"%`),");
+      }
+
+      messages.push(" cap: `"+dec(fdCap*Constants.MAX_FINAL_DAMAGE)+"` (`"+(Constants.MAX_FINAL_DAMAGE*100)+"%`)");
       respond(true,messages.join(""));
     }
   }
